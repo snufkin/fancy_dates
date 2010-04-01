1e8036f0564f129a8b6235343f2cb7bb9f1c33a9
Balazs Dianiska <balazs@longlake.co.uk>
2010-04-01 01:14:41 +0100

    Updating settings and the template.

---
+++ fancydate.tpl.php
@@ -0,0 +1,5 @@
+<div class="fancy-date">
+  <span class="day"><?php print $day; ?></span>
+  <span class="month"><?php print $month; ?></span>
+  <span class="year"><?php print $year; ?></span>
+</div>
